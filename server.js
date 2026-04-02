3befbeca8513be05045e31ee4144a865871108c8
tembosimon668-ui <tembosimon668@gmail.com>
2026-04-02 21:29:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const os = require('os');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
